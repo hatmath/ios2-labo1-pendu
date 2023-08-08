dad6684534c8c8963c3fe97f6c315fbab7e5b2c9
mathieu h <mathieuhatin2hotmail.com>
2023-08-08 14:27:10 -0400

    structure

---
+++ ios2-labo1-pendu/Model/ImagesSequenceData.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesSequenceData.swift
+//  ios2-labo1-pendu
+//
+//  Created by Mathieu Hatin (Étudiant) on 2023-08-08.
+//
+
+import Foundation
---
+++ ios2-labo1-pendu/Model/ListeDeFilmsData.swift
@@ -0,0 +1,8 @@
+//
+//  ListeDeFilmsData.swift
+//  ios2-labo1-pendu
+//
+//  Created by Mathieu Hatin (Étudiant) on 2023-08-08.
+//
+
+import Foundation
