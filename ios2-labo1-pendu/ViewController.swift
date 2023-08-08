afae02151dcfb35633b60655dce6d9875db01276
mathieu h <mathieuhatin2hotmail.com>
2023-08-08 13:59:28 -0400

    Initial Commit

---
+++ ios2-labo1-pendu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios2-labo1-pendu
+//
+//  Created by Mathieu Hatin (Étudiant) on 2023-08-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
