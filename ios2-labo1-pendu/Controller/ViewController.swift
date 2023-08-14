d7f3eb281ebb4944c477b18f04c8478b5c680e9e
Mathieu Hatin <mathieuhatin@hotmail.com>
2023-08-13 21:01:10 -0400

    intégration changement joseph

--- ios2-labo1-pendu/Controller/ViewController.swift
+++ ios2-labo1-pendu/Controller/ViewController.swift
@@ -15,64 +15,130 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblDevinette: UILabel!
     @IBOutlet weak var imgViewPendu: UIImageView!
     @IBOutlet weak var lblPointage: UILabel!
-    
+
     var pointage = 0
     var nbEssais = 7
-    var strLeMotChercher = ""
-
+    var hangmanGame: HangmanGame!
+    var movieDownloader = MovieDownloader.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         txtFieldUneLettre.text = ""
         txtFieldLesLettres.text = ""
+
+        fetchRandomMovieTitle()
+    }
+
+    func fetchRandomMovieTitle() {
+        movieDownloader.fetchRandomMovie { result in
+            switch result {
+            case .success(let movie):
+                DispatchQueue.main.async {
+                    self.hangmanGame = HangmanGame(word: movie.Title.uppercased())
+                    self.lblDevinette.text = self.hangmanGame.getGuessedWord()
+                }
+            case .failure(let error):
+                print("Error fetching movie title: \(error)")
+                // Handle error if needed
+            }
+        }
     }
 
     @IBAction func pushValider(_ sender: Any) {
-        
-        if (pointage < nbEssais) {
-            if (txtFieldUneLettre.text != "") {
-            
-                if (txtFieldLesLettres.text == ""){
-                    txtFieldLesLettres.text = txtFieldUneLettre.text
-                } else {
-                    txtFieldLesLettres.text =
-                    [txtFieldLesLettres.text,txtFieldUneLettre.text]
-                        .compactMap { $0 }
-                        .joined(separator: ",")
+        if pointage < nbEssais {
+            if let letter = txtFieldUneLettre.text?.first {
+                // Make a guess using the HangmanGame instance
+                hangmanGame.makeGuess(letter: letter)
+
+                // Update UI elements
+                txtFieldLesLettres.text = hangmanGame.getSelectedLetters().sorted().map { String($0) }.joined(separator: ", ")
+                lblPointage.text = "Pointage: \(hangmanGame.getIncorrectGuessCount())/\(nbEssais)"
+                lblDevinette.text = hangmanGame.getGuessedWord()
+
+                if hangmanGame.isWordGuessed() {
+                    // update UI
                 }
-                
-                pointage += 1
-                
-                switch pointage {
-                  case 0:
-                    lblPointage.text = "Pointage: 0/7"
-                  case 1:
-                    lblPointage.text = "Pointage: 1/7"
-                  case 2:
-                    lblPointage.text = "Pointage: 2/7"
-                  case 3:
-                    lblPointage.text = "Pointage: 3/7"
-                  case 4:
-                    lblPointage.text = "Pointage: 4/7"
-                  case 5:
-                    lblPointage.text = "Pointage: 5/7"
-                  case 6:
-                    lblPointage.text = "Pointage: 6/7"
-                  case 7:
-                    lblPointage.text = "Pointage: 7/7"
-                  default:
-                    lblPointage.text = "Pointage:"
-                }
-                           
             }
-            
-            txtFieldUneLettre.text = ""
         } else {
-        
+            // Game over
         }
-        
+
+        txtFieldUneLettre.text = ""
     }
-    
+
 }
 
+
+
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    @IBOutlet weak var txtFieldUneLettre: UITextField!
+//    @IBOutlet weak var txtFieldLesLettres: UITextField!
+//    @IBOutlet weak var btnValider: UIButton!
+//    @IBOutlet weak var lblDevinette: UILabel!
+//    @IBOutlet weak var imgViewPendu: UIImageView!
+//    @IBOutlet weak var lblPointage: UILabel!
+//
+//    var pointage = 0
+//    var nbEssais = 7
+//    var strLeMotChercher = ""
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        txtFieldUneLettre.text = ""
+//        txtFieldLesLettres.text = ""
+//    }
+//
+//    @IBAction func pushValider(_ sender: Any) {
+//
+//        if (pointage < nbEssais) {
+//            if (txtFieldUneLettre.text != "") {
+//
+//                if (txtFieldLesLettres.text == ""){
+//                    txtFieldLesLettres.text = txtFieldUneLettre.text
+//                } else {
+//                    txtFieldLesLettres.text =
+//                    [txtFieldLesLettres.text,txtFieldUneLettre.text]
+//                        .compactMap { $0 }
+//                        .joined(separator: ",")
+//                }
+//
+//                pointage += 1
+//
+//                switch pointage {
+//                  case 0:
+//                    lblPointage.text = "Pointage: 0/7"
+//                  case 1:
+//                    lblPointage.text = "Pointage: 1/7"
+//                  case 2:
+//                    lblPointage.text = "Pointage: 2/7"
+//                  case 3:
+//                    lblPointage.text = "Pointage: 3/7"
+//                  case 4:
+//                    lblPointage.text = "Pointage: 4/7"
+//                  case 5:
+//                    lblPointage.text = "Pointage: 5/7"
+//                  case 6:
+//                    lblPointage.text = "Pointage: 6/7"
+//                  case 7:
+//                    lblPointage.text = "Pointage: 7/7"
+//                  default:
+//                    lblPointage.text = "Pointage:"
+//                }
+//
+//            }
+//
+//            txtFieldUneLettre.text = ""
+//        } else {
+//
+//        }
+//
+//    }
+//
+//}
+//
